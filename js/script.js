1025b69a52b0a6a9c1fff6eb0c8414bb30feb7c3
hangchau <hanggchau098@gmail.com>
2020-11-10 14:22:26 +0700

    responsive web

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    var x = document.getElementById("menu-responsive");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous slide controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
